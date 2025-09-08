d22facbb6acce79dd6e48f4ad666eea07f724d63
Arison591 <2234819536@qq.com>
2025-09-08 19:32:36 +0800

    second save

---
+++ test1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char a[100];
+    char b[]="dian";
+    char c[]="langya";
+    char d[]="quit";
+    
+    for(int i=0;i>=0;i++){
+        printf("Please erter:");
+        scanf("%s",a);
+        if(strcmp(a,b)==0){
+        printf("2022\n");
+        }else if(strcmp(a,c)==0){
+        printf("ERROR");
+        }else if(strcmp(a,d)==0){
+        break;
+        }
+    }
+
+    return 0;
+}
